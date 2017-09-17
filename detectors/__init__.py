259d7e09f918477537ba5bd165a44c86620cfd07
Jiayu <jiayu_fibby@hotmail.com>
2017-09-17 21:52:09 +1000

    0917 remove LSH related info

--- detectors/__init__.py
+++ detectors/__init__.py
@@ -3,10 +3,5 @@
 # Implemented by Xuyun Zhang (email: xuyun.zhang@auckland.ac.nz). Copyright reserved.
 #
 
-from lsh_forest import LSHForest
 from sampling import VSSampling
 from bagging import Bagging
-from lsh import E2LSH
-from klsh import KernelLSH
-from lsh import AngleLSH
-
--- run_demo_temp.py
+++ run_demo_temp.py
@@ -8,8 +8,6 @@ import pandas as pd
 import time
 from sklearn.metrics import roc_auc_score
 from detectors import VSSampling
-from detectors import LSHForest
-from detectors import E2LSH
 from detectors import dbh_forest
 from detectors import dbh
 
@@ -33,18 +31,18 @@ ground_truth = data.as_matrix()[:, -1].tolist()
 classifiers = [
                # ("L1SH", LSHForest(num_ensemblers, VSSampling(num_ensemblers), E2LSH(norm=1))),
                ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers), dbh.DBH())),
-               ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
-                                            dbh.DBH(distance_function="Euclidean"))),
-               ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
-                                            dbh.DBH(distance_function="SquaredEuclidean"))),
-               ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
-                                            dbh.DBH(distance_function="BrayCurtis"))),
-               ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
-                                            dbh.DBH(distance_function="Canberra"))),
-               ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
-                                            dbh.DBH(distance_function="Chebyshev"))),
-               ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
-                                            dbh.DBH(distance_function="Correlation"))),
+               # ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
+               #                              dbh.DBH(distance_function="Euclidean"))),
+               # ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
+               #                              dbh.DBH(distance_function="SquaredEuclidean"))),
+               # ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
+               #                              dbh.DBH(distance_function="BrayCurtis"))),
+               # ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
+               #                              dbh.DBH(distance_function="Canberra"))),
+               # ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
+               #                              dbh.DBH(distance_function="Chebyshev"))),
+               # ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
+               #                              dbh.DBH(distance_function="Correlation"))),
                ("DBH", dbh_forest.DBHForest(num_ensemblers, VSSampling(num_ensemblers),
                                             dbh.DBH(distance_function="Cosine")))]
 
